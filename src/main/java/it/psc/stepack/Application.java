335dbfbbf8619b9e0b78c7b575ce391d4544dd97
david <david@DESKTOP-DFKC0MC.home-life.hub>
2020-01-04 18:52:45 +0100

    TODO - to complete

--- src/main/java/it/psc/stepack/Application.java
+++ src/main/java/it/psc/stepack/Application.java
@@ -6,11 +6,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 import it.psc.stepack.jpa.User;
 import it.psc.stepack.jpa.repository.UserRepository;
 
 @SpringBootApplication
+@PropertySources({ @PropertySource(value = "classpath:stepack.properties", ignoreResourceNotFound = false),
+		@PropertySource(value = "file:{}/stepack.properties", ignoreResourceNotFound = true), })
 public class Application {
 
 	public static void main(String[] args) {
